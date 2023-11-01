ba6b0d8f49463c710fa759c8bb5f45941514814d
x0f5c3 <62678514+x0f5c3@users.noreply.github.com>
2023-11-01 18:20:59 +0100

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -10,14 +10,8 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "cli-template",
-	Short: "This cli template shows the date and time in the terminal",
-	Long: `This is a template CLI application, which can be used as a boilerplate for awesome CLI tools written in Go.
-This template prints the date or time to the terminal.`,
-	Example: `cli-template date
-cli-template date --format 20060102
-cli-template time
-cli-template time --live`,
+	Use:   "gpg_keygen",
+	Short: "This software is designed to make gpg operations more ergonomic",
 	Version: "v0.0.1", // <---VERSION---> Updating this version, will also create a new GitHub release.
 	// Uncomment the following lines if your bare application has an action associated with it:
 	// RunE: func(cmd *cobra.Command, args []string) error {
@@ -57,7 +51,7 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(&pcli.DisableUpdateChecking, "disable-update-checks", "", false, "disables update checks")
 
 	// Use https://github.com/pterm/pcli to style the output of cobra.
-	pcli.SetRepo("pterm/cli-template")
+	pcli.SetRepo("x0f5c3/gpg_keygen")
 	pcli.SetRootCmd(rootCmd)
 	pcli.Setup()
 
